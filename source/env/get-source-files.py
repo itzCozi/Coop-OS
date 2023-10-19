20645e62ebc91225539e16869cccb72f9ce4ea43
Cooper <Cooperransom08@gmail.com>
2023-10-19 07:31:38 -0400

    add stuff

--- source/env/get-source-files.py
+++ source/env/get-source-files.py
@@ -1,3 +1,9 @@
 # Gets all files in source from github repo and puts them in a folder
 # for easy building so i dont have to copy and paste every thing
 # EX: git clone <repository_url>
+
+import os
+import sys
+
+def clone_repository() -> None:
+  repo_url: str = 'https://github.com/itzCozi/Coop-OS'
