872fb488238d7557d310a838f75980fd6edb3bb7
Cooper <Cooperransom08@gmail.com>
2023-10-19 07:34:56 -0400

    sorry i am rushed rn

--- source/env/get-source-files.py
+++ source/env/get-source-files.py
@@ -4,6 +4,10 @@
 
 import os
 import sys
+import shutil
 
 def clone_repository() -> None:
   repo_url: str = 'https://github.com/itzCozi/Coop-OS'
+  out_dir: str = 'C:/Users/coope/OneDrive/Desktop/Coop-OS'
+  cd_command: str = f'cd {out_dir}'
+  clone_command: str = f'git clone {repo_url}'
